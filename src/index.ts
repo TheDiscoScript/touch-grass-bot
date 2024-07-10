5ba527ced6625dbc681b21b67cd56ccfba36d060
TheDiscoScript <wollmada@gmail.com>
2024-07-10 20:48:06 +0200

    feat: init setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
